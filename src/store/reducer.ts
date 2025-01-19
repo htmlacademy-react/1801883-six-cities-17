d446e0826de69e52071051f948970c80bc9ffb05
Vladislav <vlad.4sa@gmail.com>
2025-01-19 22:46:47 +0700

    7.7. Добавлена логика проерки авторизации на сервере. Добавлен модуль работы с токеном. Зготовлены действия под login & logout.

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,10 +1,9 @@
 import { AppState } from '../types';
 import { CITIES, AuthorizationStatus, LoadingStatus } from '../consts';
-import { changeCity, changeSortType, loadFullOffer, loadNearOffers, loadComments, loadUser, setAuthorizationStatus } from './action';
-import { fetchOffers, fetchFavoriteOffers } from './api-actions';
+import { changeCity, changeSortType, loadFullOffer, loadNearOffers, loadComments } from './action';
+import { fetchOffers, fetchFavoriteOffers, checkAuthorization, login, logout } from './api-actions';
 import { createReducer } from '@reduxjs/toolkit';
 import { sortOffersByCity, sortComments } from '../utils';
-import mockData from '../mock/mock-data';
 
 const MAX_NEAR_OFFERS_NUMBER = 3;
 
@@ -41,8 +40,8 @@ export const reducer = createReducer(initialState, (builder) => {
     .addCase(fetchOffers.rejected, (state) => {
       state.loadedOffers.status = LoadingStatus.Error;
     })
-    .addCase(loadFullOffer, (state, action) => {
-      state.loadedFullOffer.data = mockData.getFullOffer(action.payload.id);
+    .addCase(loadFullOffer, () => {
+      // state.loadedFullOffer.data = mockData.getFullOffer(action.payload.id);
     })
     .addCase(fetchFavoriteOffers.pending, (state) => {
       state.loadedFavoriteOffers.status = LoadingStatus.Loading;
@@ -54,16 +53,35 @@ export const reducer = createReducer(initialState, (builder) => {
     .addCase(fetchFavoriteOffers.rejected, (state) => {
       state.loadedFavoriteOffers.status = LoadingStatus.Error;
     })
-    .addCase(loadNearOffers, (state) => {
-      state.loadedNearOffers.data = [...mockData.offers].slice(0, MAX_NEAR_OFFERS_NUMBER);
+    .addCase(loadNearOffers, () => {
+      // state.loadedNearOffers.data = [...mockData.offers].slice(0, MAX_NEAR_OFFERS_NUMBER);
     })
-    .addCase(loadComments, (state) => {
-      state.loadedComments.data = mockData.getComments().sort(sortComments);
+    .addCase(loadComments, () => {
+      // state.loadedComments.data = mockData.getComments().sort(sortComments);
     })
-    .addCase(loadUser, (state, action) => {
-      state.user.data = action.payload.user;
+    .addCase(checkAuthorization.fulfilled, (state, action) => {
+      state.user.data = action.payload;
+      state.user.status = LoadingStatus.Loaded;
+      state.authorizationStatus = AuthorizationStatus.Auth;
     })
-    .addCase(setAuthorizationStatus, (state, action) => {
-      state.authorizationStatus = action.payload.status;
+    .addCase(checkAuthorization.rejected, (state) => {
+      state.user.data = undefined;
+      state.user.status = LoadingStatus.Error;
+      state.authorizationStatus = AuthorizationStatus.NoAuth;
+    })
+    .addCase(login.fulfilled, (state, action) => {
+      state.user.data = action.payload;
+      state.user.status = LoadingStatus.Loaded;
+      state.authorizationStatus = AuthorizationStatus.Auth;
+    })
+    .addCase(login.rejected, (state) => {
+      state.user.data = undefined;
+      state.user.status = LoadingStatus.Error;
+      state.authorizationStatus = AuthorizationStatus.NoAuth;
+    })
+    .addCase(logout.fulfilled, (state) => {
+      state.user.data = undefined;
+      state.user.status = LoadingStatus.Unknown;
+      state.authorizationStatus = AuthorizationStatus.NoAuth;
     });
 });
